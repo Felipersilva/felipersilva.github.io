f85aa9e939c051f2ef2e5e61fa1d0a3238ae7114
oshiro <daniel@souldigital.com.br>
2014-11-19 13:44:42 -0200

    Thumbs e CNAME

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function () {
+	$('.slide-home').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  slide: 'div',
+	  cssEase: 'linear'
+	});
+	console.log("teste");
+});
 
---
+++ assets/js/min/main-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".slide-home").slick({dots:!0,infinite:!0,speed:500,fade:!0,slide:"div",cssEase:"linear"}),console.log("teste")});
