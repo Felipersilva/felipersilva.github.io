6fe90537034635a30a610024115383175825545e
Dobrado GrandeCastelo <daniel0oshiro@gmail.com>
2014-11-28 17:20:04 -0200

    Edições porretas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,33 @@
 $(document).ready(function () {
-	$('.slide-home').slick({
-	  dots: true,
-	  infinite: true,
-	  speed: 500,
-	  slide: 'div',
-	  easing: 'linear'
+	// $('.slide-home').slick({
+	//   dots: true,
+	//   infinite: true,
+	//   speed: 500,
+	//   slide: 'div',
+	//   easing: 'linear'
+	// });
+	// $('.slick-slide').imagefill();
+	smoothScroll.init({
+		speed: 500,
+		easing: 'quart',
+		updateURL: true,
+		offset: 0,
 	});
-	$('.slick-slide').imagefill();
+    $.scrollUp({
+        scrollName: 'scrollUp',      // Element ID
+        scrollDistance: 300,         // Distance from top/bottom before showing element (px)
+        scrollFrom: 'top',           // 'top' or 'bottom'
+        scrollSpeed: 300,            // Speed back to top (ms)
+        easingType: 'linear',        // Scroll to top easing (see http://easings.net/)
+        animation: 'fade',           // Fade, slide, none
+        animationSpeed: 200,         // Animation speed (ms)
+        scrollTrigger: false,        // Set a custom triggering element. Can be an HTML string or jQuery object
+        scrollTarget: false,         // Set a custom target element for scrolling to. Can be element or number
+        scrollText: '<i class="fa fa-caret-square-o-up"></i>', // Text for element, can contain HTML
+        scrollTitle: false,          // Set a custom <a> title if required.
+        scrollImg: false,            // Set true to use image
+        activeOverlay: false,        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+        zIndex: 2147483647           // Z-Index for the overlay
+    });
 });
 
--- assets/js/min/main-min.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".slide-home").slick({dots:!0,infinite:!0,speed:500,fade:!0,slide:"div",cssEase:"linear"})});
